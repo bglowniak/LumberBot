267b2503d9ad558c15184b984668735004ef9557
Brian Glowniak <bglowniak@gmail.com>
2020-07-23 20:33:15 -0500

    Rework LumberBot to subclass commands.Bot. Begin to add triple joke to on_message

--- LumberBot.py
+++ LumberBot.py
@@ -1,27 +1,35 @@
 import discord
-import os
+from discord.ext.commands import Bot, command, Cog
 from dotenv import load_dotenv
+import os
 import random
 import re
 
-class LumberBot(discord.Client):
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
-        load_dotenv()
-        self.mention_id = os.getenv("BOT_MENTION_ID")
+class LumberBot(Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.greetings = ["hello", "hi", "hiya", "hey", "howdy",
                           "sup", "hola", "privet", "salve", "ciao",
                            "konnichiwa", "shalom"]
+        load_dotenv()
+        self.mention_id = os.getenv("BOT_MENTION_ID")
 
+        self.add_command(self.placeholder)
+
+    # EVENTS
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+    # override on_message to implement some functionality outside of normal commands
     async def on_message(self, message):
-        # first check if the message author is our LumberBot or another bot
         if message.author == self.user or message.author.bot:
             return
 
         content = message.content.strip().lower()
         author_mention = "<@!" + str(message.author.id) + ">"
 
-        # check if the message @s our bot
+        # check if the message @s our bot and greets it. Respond with a random greeting
+        # I didn't make this a separate command because this way allows to ignore punctuation
         if content.startswith(self.mention_id):
             # check for greeting
             greeting_patterns = "|".join(self.greetings)
@@ -30,13 +38,24 @@ class LumberBot(discord.Client):
                 response = author_mention + " " + random_greeting + "!"
                 await message.channel.send(response)
 
-        # check if the message includes "big _______" and respond accordingly
+        '''# check if the message includes "big _______" and respond accordingly
         next_word = self.check_for_big(content)
         if next_word != None:
             response = author_mention + " what kind of " + next_word + "?"
             await message.channel.send(response)
         else:
-            print("No Big Detected")
+            print("No Big Detected")'''
+
+        if "trip" in content:
+            await message.channel.send("trip? triple? triplexlink??")
+
+        await self.process_commands(message)
+
+    # COMMANDS
+    @command(name="placeholder")
+    async def placeholder(ctx):
+        print("placeholder")
+        pass
 
     # HELPERS
     def check_for_big(self, message):
@@ -44,3 +63,6 @@ class LumberBot(discord.Client):
         for index, word in enumerate(words):
             if word == "big" and index + 1 != len(words):
                 return words[index + 1]
+
+    def load_salute(self):
+        pass
--- load-bot.py
+++ load-bot.py
@@ -1,10 +1,11 @@
 import os
 from LumberBot import LumberBot
 from dotenv import load_dotenv
+from discord.ext import commands
 
 if __name__ == "__main__":
     load_dotenv()
     TOKEN = os.getenv("DISCORD_TOKEN")
 
-    client = LumberBot()
-    client.run(TOKEN)
+    bot = LumberBot(command_prefix=commands.when_mentioned_or("!"))
+    bot.run(TOKEN)
