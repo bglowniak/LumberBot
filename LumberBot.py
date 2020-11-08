60660f9fe14f4a09071e5266060a16ec7eb2d7f0
Brian Glowniak <bglowniak@gmail.com>
2020-11-07 20:22:53 -0500

    Replace threading with discord task. Implement match check and stats message for test win case

--- LumberBot.py
+++ LumberBot.py
@@ -1,10 +1,10 @@
 import discord
-from discord.ext.commands import Bot, command, Cog
+from discord.ext import tasks
+from discord.ext.commands import Bot, command
 from dotenv import load_dotenv
 import os
 import random
 import re
-import threading
 import requests
 
 class LumberBot(Bot):
@@ -27,7 +27,7 @@ class LumberBot(Bot):
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
         self.general_channels = self.collect_general_channels()
-        threading.Thread(target=self.warzone_win_tracker, name="warzone_win_tracker").start()
+        self.check_warzone_wins.start()
 
     # override on_message to implement some functionality outside of normal commands
     async def on_message(self, message):
@@ -64,6 +64,52 @@ class LumberBot(Bot):
         # once we have checked the full message, process any commands that may be present
         await self.process_commands(message)
 
+
+    @tasks.loop(minutes=30.0)
+    async def check_warzone_wins(self):
+        api_session = self.authenticate_warzone_api(self.cod_email, self.cod_pw)
+
+        base_URL = "https://my.callofduty.com/api/papi-client/"
+        req_URL = base_URL + "crm/cod/v2/title/mw/platform/uno/gamer/" + self.cod_username + "/matches/wz/start/0/end/0/details"
+
+        #most_recent_match_id = None
+        most_recent_match_id = "6359357863122988582"
+
+        # TO-DO: Add loop so the code below executes every 15 (or 30?) minutes
+        # TO-DO: Add better logging (checking matches, number of matches checked, etc.)
+
+        # this API request will return Warzone matches played in the last week. Although there is a start and end in the
+        # URL, they don't work as expected and there is very little documentation as to what these attributes do.
+        recent_matches = api_session.get(req_URL).json()["data"]["matches"]
+
+        # the purpose of most_recent_match_id is to make sure we only process new matches added to the list (despite
+        # pulling the full list each time)
+        if most_recent_match_id == None:
+            most_recent_match_id = recent_matches[0]["matchID"]
+        elif recent_matches[0]["matchID"] != most_recent_match_id: # there are new matches to process
+            for match in recent_matches:
+                current_ID = match["matchID"]
+                if current_ID == most_recent_match_id: # we have processed all new matches in the list and thus can stop
+                    break
+
+                placement = match["playerStats"]["teamPlacement"]
+                if placement == 1: # we won a match
+                    print("Win spotted!")
+                    team = match["player"]["team"]
+                    match_url = base_URL + "crm/cod/v2/title/mw/platform/uno/fullMatch/wz/" + current_ID + "/en"
+                    match_data = api_session.get(match_url).json()
+
+                    team_stats = {}
+
+                    for player in match_data["data"]["allPlayers"]:
+                        if player["player"]["team"] == team:
+                            team_stats[player["player"]["username"]] = player["playerStats"]["kills"]
+
+                    stats = self.format_stats(team_stats)
+                    await self.general_channels["Bot Test Server"].send("Congratulations on your recent win!  \nMatch Time:  \nTeam Kills:  \n" + stats)
+
+            most_recent_match_id = recent_matches[0]["matchID"]
+
     # COMMANDS
     @command(name="tell")
     async def tell(ctx, arg1, *args):
@@ -88,6 +134,14 @@ class LumberBot(Bot):
                     channels[guild.name] = channel
 
         return channels
+
+    # returns markdown formatted list of players and their match kills
+    def format_stats(self, player_dict):
+        format = ""
+        for player, kills in player_dict.items():
+            format += "    • " + player + ": " + str(int(kills)) + " kills  \n"
+
+        return format
 
     # TO-DO: Add checks for when these requests don't return a status code of 200
     def authenticate_warzone_api(self, email, password):
@@ -114,6 +168,3 @@ class LumberBot(Bot):
             print("API Session successfully authenticated")
 
         return api_session
-
-    def warzone_win_tracker(self):
-        api_session = self.authenticate_warzone_api(self.cod_email, self.cod_pw)
